3fc6df4be29e4da15c9d6521979af5153d15c046
μT <37547640+mutichung@users.noreply.github.com>
2025-06-22 12:44:57 +0000

    Impelement dequantize function; bug fixes; add test.

---
+++ tests/test_q2k.py
@@ -0,0 +1,14 @@
+import torch
+
+from ggml_quants_py import ggml_quants
+
+
+def test_quantize_row_q2k():
+    x = torch.randn((1024,))
+    qx = ggml_quants.quantize_row_q2_K_impl(x, torch.ones_like(x))
+    dq_x = ggml_quants.dequantize_row_q2_K(qx)
+    assert dq_x.shape == x.shape
+    dq_x = dq_x.reshape(-1, 16)
+    x = x.reshape(-1, 16)
+    for x_block, block in zip(x, dq_x):
+        assert torch.unique(block).numel() <= 4, "Block should be quantized to 2 bits."
