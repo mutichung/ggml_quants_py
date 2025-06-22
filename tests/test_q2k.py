1512930609c14e73b36b58e6d98b027cbe6d2d3f
μT <37547640+mutichung@users.noreply.github.com>
2025-06-22 15:24:18 +0000

    Implement quantize and dequantize function; add basic tests.

--- tests/test_q2k.py
+++ tests/test_q2k.py
@@ -12,3 +12,16 @@ def test_quantize_row_q2k():
     x = x.reshape(-1, 16)
     for x_block, block in zip(x, dq_x):
         assert torch.unique(block).numel() <= 4, "Block should be quantized to 2 bits."
+
+
+def test_quantize_q2k():
+    x = torch.randn((4, 1024))
+    qx = ggml_quants.quantize_q2_K(x, torch.ones_like(x))
+    assert x.size(0) == len(qx)
+    assert x.size(1) // ggml_quants.QK_K == len(qx[0])
+
+
+def test_dequantize_q2k():
+    x = torch.randn((4, 1024))
+    dqx = ggml_quants.dequantize_q2_K(ggml_quants.quantize_q2_K(x, torch.ones_like(x)))
+    assert x.size() == dqx.size()
